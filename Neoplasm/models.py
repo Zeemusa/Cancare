51d65ccc74753c814daf84ff7e545cd4350ee364
haimana <haimanaare@gmail.com>
2024-12-13 01:16:04 +0300

    created types template

--- Neoplasm/models.py
+++ Neoplasm/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Cancer(models.Model):
+    type_of_cancer = models.CharField(max_length=35)
+    description = models.CharField(max_length=3000)
+    symptoms = models.CharField(max_length=5000)
+    treatment = models.CharField(max_length=5000)
+
+    def __str__(self):
+        return self.type_of_cancer
+    
+
+class Post(models.Model):
+    msg_description = models.CharField(max_length=5000)
+    period = models.IntegerField()
+    date = models.DateTimeField(default= datetime.datetime.today)
+
+    def __str__(self):
+        return self.msg_description
+
+
+
+
--- Neoplasm/views.py
+++ Neoplasm/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def homepage(request):
     return render(request, 'index.html')
+
+
+def types(request):
+    types = Cancer.objects.all()
+    return render(request,'type.html',{'types':types})
