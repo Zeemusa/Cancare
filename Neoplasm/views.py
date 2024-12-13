aa0ee79b4cae08cd500c4cd14876349688e5dd8f
haimana <haimanaare@gmail.com>
2024-12-13 06:47:18 +0300

    added login template and functionality

--- Neoplasm/views.py
+++ Neoplasm/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import *
 from .form import *
-from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth import authenticate, login ,logout
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
@@ -30,9 +30,29 @@ def register_user(request):
             #log in user 
             user = authenticate(username = username, password = password)
             login(request,user)
-            return redirect('home')
+            return redirect('login')
         else:
             # messages.success(request("Whoops, There was a problem, Please try again"))
             return redirect('register')
     else:
         return render(request, 'register.html', {'form': form})
+    
+def login_user(request):
+    if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password = password)
+        if user is not None:
+            login(request,user)
+            # messages.success(request("You Have Been Loged In"))
+            return redirect('types')
+        else:
+            # messages.success(request("There Was An Error, Please Try Again"))
+            return redirect('login') 
+         
+    else:
+        return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('homepage')
