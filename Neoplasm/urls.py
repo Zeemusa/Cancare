5398caf2eb994b0845e07bbe1e6460ea336ff0f4
haimana <haimanaare@gmail.com>
2024-12-13 05:22:58 +0300

    created register template and added functionality

--- Neoplasm/urls.py
+++ Neoplasm/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
    path('', views.homepage, name='homepage'),
    path('cancer/',views.cancer, name='types'),
-   path('cancer/<int:pk>', views.disease, name='disease')
+   path('cancer/<int:pk>', views.disease, name='disease'),
+   path('register/', views.register_user, name='register'),
+   path('login/', views.login_user, name='login'),
+   path('logout/', views.logout_user, name='logout'),
 ]
